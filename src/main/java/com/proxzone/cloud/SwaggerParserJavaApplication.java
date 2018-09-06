2a574e9e1c23504b80531147af8ddca8e3b6cb36
Mayanbin <mayanbin@proxzone.com>
2018-09-06 12:01:36 +0800

    add project first

--- src/main/java/com/proxzone/cloud/SwaggerParserJavaApplication.java
+++ src/main/java/com/proxzone/cloud/SwaggerParserJavaApplication.java
@@ -1,30 +1,29 @@
 package com.proxzone.cloud;
+
+
+import io.swagger.models.*;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.v3.parser.util.ClasspathHelper;
+
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.swagger.models.HttpMethod;
-import io.swagger.models.Info;
-import io.swagger.models.Operation;
-import io.swagger.models.Path;
-import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
-import io.swagger.parser.util.ClasspathHelper;
 /**
  * @author mayanbin@proxzone.com
  * @version 1.0
  * @date 18-9-5 下午1:12
  */
-@SpringBootApplication
+
 public class SwaggerParserJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwaggerParserJavaApplication.class, args);
+       // SpringApplication.run(SwaggerParserJavaApplication.class, args);
+     //   Swagger swagger = new SwaggerParser().parse("petstore-swagger.yml");
 
-        String swaggerAsString = ClasspathHelper.loadFileFromClasspath("petstore-swagger.json");
+        String swaggerAsString = ClasspathHelper.loadFileFromClasspath("petstore-swagger.yml");
         // read a swagger description from the petstore
         Swagger swagger = new SwaggerParser().parse(swaggerAsString);
 
@@ -45,15 +44,40 @@ public class SwaggerParserJavaApplication {
         for (Iterator<String> iterator = keys.iterator(); iterator.hasNext();) {
             String key = iterator.next();
             Path path = paths.get(key);
-            System.out.println("" + key);
+            System.out.println("url:" + key);
 
             Map<HttpMethod, Operation> operationMap = path.getOperationMap();
             Set<HttpMethod> operationsKeySet = operationMap.keySet();
             for (Iterator<HttpMethod> operIterator = operationsKeySet.iterator(); operIterator.hasNext();) {
                 HttpMethod httpMethod = operIterator.next();
-                // Operation operation = operationMap.get(httpMethod);
+                 Operation operation = operationMap.get(httpMethod);
+                System.out.println("http:" + httpMethod);
+                System.out.println(" tags :"+operation.getTags());
+                System.out.println("description:	" + operation.getDescription());
+                System.out.println("  operationId : "+operation.getOperationId());
+                System.out.println(" summary:	" + operation.getSummary());
+                System.out.println("  consumes:  "+operation.getConsumes());
+                System.out.println("   produces:	" + operation.getProduces());
+                List<Parameter> parameterList = operation.getParameters();
+                for (int i=0;i<parameterList.size();i++){
+                    System.out.println("  in:  "+parameterList.get(i).getIn());
+                    System.out.println("  name:  "+parameterList.get(i).getName());
+                    System.out.println("  description:  "+parameterList.get(i).getDescription());
+                    System.out.println("  required:  "+parameterList.get(i).getRequired());
 
-                System.out.println("	" + httpMethod);
+                }
+                Map<String, Response> responseMap = operation.getResponses();
+                System.out.println("response:" + responseMap);
+                System.out.println(" security" + operation.getSecurity());
+
+
+
+
+
+
+
+
+
 
             }
         }
